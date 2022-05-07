d29691b1926c7255ed9d746acce4503f14067ece
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:20:32 -0500

    AdvancedHome class populated

--- src/main/java/vaskarans/decorator/AdvancedHome.java
+++ src/main/java/vaskarans/decorator/AdvancedHome.java
@@ -1,4 +1,17 @@
 package vaskarans.decorator;
 
-public class AdvancedHome {
+public class AdvancedHome extends Home {
+
+    public AdvancedHome() {
+        additionalCost = 25_000;
+        System.out.println(" It becomes an advanced home with more facilities..");
+        System.out.println(" You need to pay $" + this.getPrice() + " for this.");
+
+    }
+
+    @Override
+    public double getPrice() {
+        return basePrice + additionalCost;
+    }
+
 }
