7636999b95429e4f9fc1d0c0d51253b7245dbc42
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:24:53 -0500

    SwimmingPool class populated

--- src/main/java/vaskarans/decorator/SwimmingPool.java
+++ src/main/java/vaskarans/decorator/SwimmingPool.java
@@ -1,4 +1,15 @@
 package vaskarans.decorator;
 
-public class SwimmingPool {
+public class SwimmingPool extends Luxery{
+    public SwimmingPool(Home home) {
+        super(home);
+        this.luxuryCost = 55_000;
+        System.out.println(" For a swimming pool, you pay an extra $" + this.luxuryCost);
+    }
+
+    @Override
+    public double getPrice(){
+        return home.getPrice() + luxuryCost;
+    }
+
 }
