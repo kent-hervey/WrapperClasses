170a8bae03dd282ba6383dae0e9f4686ccf9bde7
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:35:28 -0500

    BasicHome class populated

--- src/main/java/vaskarans/decorator/BasicHome.java
+++ src/main/java/vaskarans/decorator/BasicHome.java
@@ -1,4 +1,15 @@
 package vaskarans.decorator;
 
-public class BasicHome {
+public class BasicHome extends Home {
+    public BasicHome(){
+    // No additional cost for a basic home
+        System.out.println(" The basic home with some stanard facilities is ready.");
+        System.out.println(" You need to pay $" + this.getPrice() + " for this.");
+
+    }
+
+    @Override
+    public double getPrice() {
+        return basePrice;
+    }
 }
