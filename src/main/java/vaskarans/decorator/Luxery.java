d410d0ba9c291bf0c1e3cb2f00b2fd6effa58a7c
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:03:41 -0500

    Luxery class populated

--- src/main/java/vaskarans/decorator/Luxery.java
+++ src/main/java/vaskarans/decorator/Luxery.java
@@ -1,4 +1,17 @@
 package vaskarans.decorator;
 
-public class Luxery {
+public class Luxery extends Home {
+
+    protected Home home;
+
+    public double luxuryCost;
+
+    public Luxery(Home home) {
+        this.home = home;
+    }
+
+    @Override
+    public double getPrice() {
+        return home.getPrice();
+    }
 }
