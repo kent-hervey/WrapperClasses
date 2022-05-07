41df3127c8806b92dfca442f4169e26150662a04
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 11:59:40 -0500

    Home class populated

--- src/main/java/vaskarans/decorator/Home.java
+++ src/main/java/vaskarans/decorator/Home.java
@@ -1,4 +1,16 @@
 package vaskarans.decorator;
 
-public class Home {
+abstract class Home {
+    public double basePrice;
+    public double additionalCost;
+
+    public Home() {
+        // Minimum home price is $100,000
+        this.basePrice = 100000.0;
+        this.additionalCost = 0.0;
+    }
+
+    // public abstract void buildHome();
+    public abstract double getPrice();
+
 }
