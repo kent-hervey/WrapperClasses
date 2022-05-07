710b09c8721eb6025b6086045849ef810be49239
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:36:52 -0500

    Scenario 1 populated

--- src/main/java/vaskarans/decorator/Client.java
+++ src/main/java/vaskarans/decorator/Client.java
@@ -1,4 +1,15 @@
 package vaskarans.decorator;
 
 public class Client {
+    public static void main(String[] args) {
+        System.out.println("Time to Decorate");
+        System.out.println();
+
+        System.out.println("***Using wrappers in different scenarios.***\n");
+        System.out.println("Scenario-1: Making a basic home with standard facilities.");
+        Home home = new BasicHome();
+        System.out.println("Total cost: $" + home.getPrice());
+
+    }
+
 }
