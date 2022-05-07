d60be099ec03b6cc88a3ef183561c444f5ed32b2
Kent Hervey <kent.hervey@gmail.com>
2022-05-07 12:16:45 -0500

    Playground class populated

---
+++ src/main/java/vaskarans/decorator/Playground.java
@@ -0,0 +1,15 @@
+package vaskarans.decorator;
+
+public class Playground extends Luxery {
+    public Playground(Home home) {
+        super(home);
+        this.luxuryCost = 20_000;
+        System.out.println(" For a playground, you pay an extra$" + this.luxuryCost);
+    }
+
+    @Override
+    public double getPrice() {
+        return home.getPrice() + luxuryCost;
+    }
+}
+
